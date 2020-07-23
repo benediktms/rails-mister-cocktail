b49d46d45076c5d546b205fc23f46472058fb8b1
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-07-23 20:37:32 +0100

    added basic controller functionality

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,26 @@
 class DosesController < ApplicationController
+  before_action :strong_params, only: :create
+  before_action :set_cocktail, only: %i[new create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to(@dose)
+    else
+      render :new
+    end
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def strong_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
